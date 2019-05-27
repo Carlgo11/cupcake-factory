741df852244f936bc78ad680671b888998b3f6af
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-05-27 22:09:33 +0200

    Lazy load some images and add favicon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $('#scroll-btn').click(function () {
     document.getElementById("div-one").scrollIntoView({behavior: "smooth"});
 });
+
+$('img')
+    .visibility({
+        type: 'image',
+        transition: 'fade in',
+        duration: 1000
+    });
